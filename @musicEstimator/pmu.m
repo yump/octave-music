c0d643700106be157e6b194971f9cff25539cc17
Russell Haley <rhaley@lily>
2013-02-06 02:02:57 -0600

    Removed unncessary print statements and comments.

--- @musicEstimator/pmu.m
+++ @musicEstimator/pmu.m
@@ -3,13 +3,9 @@ function [result] = pmu(music, propvec)
 	% propvec is a matrix holding unit vectors from the 
 	% dirctions of interest.  (FROM the directions, remember.)
 
-	printf("in pmu\n");
 	insz = size(propvec);
 	noisetemp = music.noisespace * music.noisespace'; %subexp
 	arraypartemp = music.arraypar';
-
-	%printf("Size of propvec\n");
-	%insz
 
 	% single point case (propvec is 3x1)
 	if ( (length(insz) == 2) && all(insz == [3 1]) )
