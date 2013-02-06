bc6f3c98ec9b318987e2dd78374daa481b1c1cd3
Russell Haley <rhaley@tamu.edu>
2013-02-05 23:36:44 -0600

    Added display.m and doa.m to git.

---
+++ @musicEstimator/display.m
@@ -0,0 +1,18 @@
+function display (music)
+
+	printf("samples\n")
+	music.samples
+
+	printf("arraypar\n")
+	music.arraypar
+
+	printf("covar\n")
+	music.covar
+
+	printf("noisedim\n")
+	music.noisedim
+
+	printf("noisespace\n")
+	music.noisespace
+
+endfunction
