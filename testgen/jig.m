9137e4cecfed1903ae3dbedd7135125a0cfcb9c7
Russell Haley <rhaley@lily>
2013-02-18 02:33:35 -0600

    Now working with 18 element random array.

--- testgen/jig.m
+++ testgen/jig.m
@@ -14,7 +14,9 @@ antennas = [
 	0  0 -1
 	0 -1 -1
 ];
-antennas = antennas' ./8;
+%antennas = antennas' ./8;
+
+load randtennas.dat
 
 
 s1 = [-1 0 0]';                        %straight on
@@ -22,18 +24,16 @@ s3 = sph2cart([pi/2*(rand - 0.5), pi/2*(rand -0.5), 1])';
 s2 = sph2cart([pi/2*(rand - 0.5), pi/2*(rand -0.5), 1])';
 s4 = sph2cart([pi/2*(rand - 0.5), pi/2*(rand -0.5), 1])';
 
-numsamples = 1;
+numsamples = 128;
 noise = 0.0005;
 sample1 = testgen(antennas, repmat(s1,[1,numsamples]), noise);
 sample2 = testgen(antennas, repmat(s2,[1,numsamples]), noise);
 sample3 = testgen(antennas, repmat(s3,[1,numsamples]), noise);
 sample4 = testgen(antennas, repmat(s4,[1,numsamples]), noise);
-sample = sample1 + sample2;
+sample = sample2;
 %sample = sample2;
 
 estimator = musicEstimator(antennas, sample);
-abs(pmu(estimator, s1))
-abs(pmu(estimator, [0 1 0]'))
 
 %tic;
 %spectemp = pseudospec(estimator, 2*pi, pi/2, 128, 128);
@@ -48,9 +48,15 @@ tic;
 spectrum = incident(estimator, tt, pp);
 toc;
 
-%[xx, yy, zz] = sph2cart(tt,pp,spectrum);
+prettyspectrum = imadjust(spectrum,stretchlim(spectrum,0)) + 0.2;
+
+[xx, yy, zz] = sph2cart(tt,pp,prettyspectrum);
 figure(2);
-mesh(spectrum);
+%surf(xx,yy,zz);
+%axis([-1 1 -1 1 -1 1]*(1+0.2));
+mesh(tt,pp,spectrum);
+xlabel('theta');
+ylabel('phi');
 print("spectrum.eps")
 
 imwrite(imadjust(spectrum,stretchlim(spectrum,[0,1])), "spectrum.png");
