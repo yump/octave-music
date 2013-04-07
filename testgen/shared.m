56e8478c3fa67d5af91f04c3da1ed8b5c571dc51
Russell Haley <rhaley@tamu.edu>
2013-04-07 13:47:22 -0500

    Smaller steps on the rotating signal.

--- testgen/shared.m
+++ testgen/shared.m
@@ -10,7 +10,7 @@ s2 = -sph2cart([pi/4*(rand - 0.5), pi/4*(rand -0.5), 1])';
 s4 = -sph2cart([pi/4*(rand - 0.5), pi/4*(rand -0.5), 1])';
 
 %rotating signal
-th = pi/8;
+th = pi/16;
 rot = [cos(th) -sin(th) 0; sin(th) cos(th) 0; 0 0 1];
 if ( ! exist('s5') )
 	s5 = s1;
@@ -19,14 +19,14 @@ end
 s5 = rot*s5;
 %cart2sph(s5')
 
-numsamples = 32;
+numsamples = 16;
 snr=20;
 sample1 = testgen(antennas, s1, numsamples, snr);
 sample2 = testgen(antennas, s2, numsamples, snr);
 sample3 = testgen(antennas, s3, numsamples, snr);
 sample4 = testgen(antennas, s4, numsamples, snr);
 sample5 = testgen(antennas, s5, numsamples, snr);
-sample = sample2 + sample1;
+sample = sample5 + sample1;
 
 estimator = musicEstimator(antennas, sample, 2);
 
